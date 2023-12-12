d6766a20a80a48629a8cf4a422d9b491eb5a2d0b
TobiaRigon <rigontobia@gmail.com>
2023-12-12 15:23:54 +0100

    Feat: Milestone 1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
+// Stampare all’interno di una lista HTML un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+
+// Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+
+// Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
+
+
 const { createApp } = Vue
 
 createApp({
   data() {
     return {
-      
-      
+      message: "",
+
+      todos: [
+        {
+            text: 'Fare i compiti',
+            done: false
+        },
+        {
+            text: 'Fare la spesa',
+            done: true
+        },
+        {
+            text: 'Fare il bucato',
+            done: false
+        }
+    ],
+
     }
   }
 }).mount('#app')
